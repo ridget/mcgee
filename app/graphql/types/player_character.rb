00112c493a220b65827793dc510947b20f217d6c
Tom Ridge <thomas.ridge@cultureamp.com>
2018-12-30 20:45:43 +1000

    adjustments for slides

--- app/graphql/types/player_character.rb
+++ app/graphql/types/player_character.rb
@@ -1,7 +1,7 @@
 class Types::PlayerCharacter < GraphQL::Schema::Object
   field :id, ID, null: false
   field :name, String, null: false
-  field :class, Types::PlayerClass, null: false, resolve: -> (_o, _c, _a) do
-    OpenStruct.new(level: 3, name: "WIZARD")
-  end
+  field :hair, String, null: true
+  field :eyes, String, null: true
+  field :age, Int, null: true
 end
--- app/graphql/types/query.rb
+++ app/graphql/types/query.rb
@@ -1,7 +1,5 @@
 class Types::Query < GraphQL::Schema::Object
   field :player_character, Types::PlayerCharacter, null: false, resolve: -> (_o, _c, _a) do
-    OpenStruct.new(id: 1, name: "Mcgee")
+    OpenStruct.new(id: 1, name: "Mcgee", hair: "black", eyes: "copper")
   end
-
-  field :playable_classes, resolver: Resolvers::PlayableClasses
 end
